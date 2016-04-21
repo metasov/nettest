3ebfe124c2447308beec4cb0aa8f99782b8b4bab
Metasov Artur <metasov@gmail.com>
2016-04-21 02:38:06 -0400

    Add setup.py, move files to package, add .gitignore

---
+++ setup.py
@@ -0,0 +1,17 @@
+#/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='nettest',
+    version='0.0.1',
+    description='Utility to measure network service SLA',
+    author='Metasov Arthur',
+    author_email='metasov@gmail.com',
+    url='https://github.com/metasov/nettest',
+    packages=['nettest'],
+    install_requires=[
+        'pyroute2>0.3.21',
+    ]
+)
+
