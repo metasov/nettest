1d6cb10a878c41ec14f1baaba045ea5d3b60fde2
Metasov Artur <metasov@gmail.com>
2016-07-05 17:22:38 -0400

    Add logging to dns check

--- nettest/dns.py
+++ nettest/dns.py
@@ -1,13 +1,21 @@
+import logging
 import socket
 import time
 
 def test_dns(config):
+    log = logging.getLogger(__name__)
     domain = config.get('dns.domain')
+    log.info('Trying to resolve dns name %s', domain)
     start_time = time.time()
     try:
         socket.gethostbyname(domain)
-    except socket.error:
+    except socket.error as e:
+        log.error(
+            'Cannot resolve name: %s',
+            traceback.format_exception_only(
+                e.__class__.__name__, e))
         return None
     stop_time = time.time()
+    log.info('Time: %.3f sec.', stop_time - start_time)
     return stop_time - start_time
 
